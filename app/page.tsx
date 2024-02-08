f1c827f852208fc1ff5246e098a9be675af982ed
MrOrz <johnsonliang7@gmail.com>
2024-02-08 14:18:58 +0800

    fix: try making vercel happy by disabling statics

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { request, gql } from 'graphql-request';
 import { graphql } from '@/typegen/gql';
 
-function getData() {
+async function getData() {
+  'use server';
   return request(process.env.COFACTS_API_URL ?? '', graphql(`
     query LoadAPIStats {
       allArticles: ListArticles {
