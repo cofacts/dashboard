7d300cfb4192114c5f4b3f3c753aa6996d1aa20d
MrOrz <johnsonliang7@gmail.com>
2024-02-08 14:33:53 +0800

    fix: allow syntax highlight on graphql items

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { request, gql } from 'graphql-request';
+import { request } from 'graphql-request';
 import { graphql } from '@/typegen/gql';
 
 async function getData() {
-  return request(process.env.COFACTS_API_URL ?? '', graphql(`
+  return request(process.env.COFACTS_API_URL ?? '', graphql(/* GraphQL */ `
     query LoadAPIStats {
       allArticles: ListArticles {
         totalCount
